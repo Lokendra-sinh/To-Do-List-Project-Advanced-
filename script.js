570315fbbcac7c35354e23d9d986dfc0ad0e414d
Lokendra <anizen242@gmail.com>
2023-07-01 21:10:45 -0700

    Improvised to-do list app. Features added like css-animations,keyframes, interactive elements etc.  But the app still lacking storage feature...

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
-const addTaskButton = document.querySelector('.addButton');
+  const addTaskButton = document.querySelector('.addButton');
 
-addTaskButton.addEventListener('click', addNewTask);
+  addTaskButton.addEventListener('click', addNewTask);
 
-function addNewTask(event) {
-  const inputText = document.querySelector('#taskInput');
-  const inputTextValue = inputText.value;
+  function addNewTask(event) {
+    const inputText = document.querySelector('#taskInput');
+    const inputTextValue = inputText.value;
 
-  if (inputTextValue.trim() === '') {
-    alert('Please enter a task!');
-    return; // Exit the function if the input is empty
+    if (inputTextValue.trim() === '') {
+      alert('Please enter a task!');
+      return; // Exit the function if the input is empty
+    }
+
+    const newTask = document.createElement('li');
+    
+
+    const taskDetails = document.createElement('span');
+    taskDetails.textContent = inputTextValue;
+
+    const taskCheckbox = document.createElement('input');
+    taskCheckbox.type = 'checkbox';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+
+    newTask.classList.add('slideIn');
+    // deleteButton.textContent = 'Delete';
+
+    deleteButton.addEventListener('click', function() {
+      newTask.classList.add('slideOut');
+      setTimeout(function() {
+        newTask.remove();
+      }, 500);
+    });
+    
+    newTask.appendChild(taskDetails);
+    newTask.appendChild(taskCheckbox);
+    newTask.appendChild(deleteButton);
+
+    const taskListContainer = document.querySelector('ul');
+
+    taskListContainer.appendChild(newTask);
+    inputText.value = "";
   }
-
-  const newTask = document.createElement('li');
-  const taskDetails = document.createElement('span');
-  taskDetails.textContent = inputTextValue;
-
-  const deleteButton = document.createElement('button');
-  deleteButton.textContent = 'Delete';
-
-  deleteButton.addEventListener('click', function() {
-    newTask.remove();
-  });
-  taskDetails.appendChild(deleteButton);
-  newTask.appendChild(taskDetails);
-
-  const taskListContainer = document.querySelector('ul');
-
-  taskListContainer.appendChild(newTask);
-  inputText.value = "";
-}
